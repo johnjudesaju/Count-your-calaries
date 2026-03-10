1bee9faaa4db4d79fdbb148341c1d164d25e5f01
johnjudesaju <johnjudesaju93@gmail.com>
2026-03-10 16:18:52 +0530

    calorie

--- count_your_calories/count_your_calories/app_core/views.py
+++ count_your_calories/count_your_calories/app_core/views.py
@@ -9,6 +9,7 @@ from app_dashboard.models import Customer, Deliverydetails
 from count_your_calories.users.models import User
 from django.http import JsonResponse
 from django.template.loader import render_to_string
+from django.contrib.auth.decorators import login_required
 
 #from count_your_calories.app_dashboard.models import customer
 #from count_your_calories.users.models import User
@@ -295,7 +296,7 @@ def delv_v(request):
     l=Delivery.objects.filter(status="accept")
     return render (request, "delvv.html",{"dv":l})
 
-# 
+@login_required
 def cust_smoothieview(request):
     # Category filtering
     
@@ -359,6 +360,7 @@ def cust_smoothieview(request):
         }
     )
 
+@login_required
 def custz_smoothie(request):
     i=Ingredients.objects.all()
     search=request.GET.get('search')
@@ -366,7 +368,7 @@ def custz_smoothie(request):
 
     if search:
         i = i.filter(name__icontains=search)
-        
+
     if sort == "price_asc":
         i = i.order_by('price')
 
@@ -396,7 +398,8 @@ def custz_smoothie(request):
     cn=CustomIngredients.objects.filter(customer=request.user,customMaster_id__isnull=True).count()
     total_amount = cr.aggregate(total=Sum('price'))['total'] or 0
     return render (request, "customize_smoothie.html",{"custzview":i,"cr":cr,"cn":cn,"t":total_amount})
-    
+
+@login_required    
 def add_fav(request,no):
     f=Favourite()
     f.customer=request.user
@@ -404,6 +407,7 @@ def add_fav(request,no):
     f.save()
     return HttpResponse("<script>alert('added to fav');window.location='/core/cust_smoothie';</script>")
 
+@login_required
 def fav(request):
     f=Favourite.objects.filter(customer=request.user)
     cn=Favourite.objects.filter(customer=request.user).count()
